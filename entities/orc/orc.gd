1ee2e64527f51682bb5ecb6bdcb11e25e93d9a1c
Guilherme Marcondes <guilherme.marcondes@tecnico.ulisboa.pt>
2022-12-26 16:37:46 +0000

    :sparkles: Trying to make ai work

--- entities/orc/orc.gd
+++ entities/orc/orc.gd
@@ -1,3 +1,7 @@
 extends CharacterBody2D
 class_name Orc
 
+@export @onready var chase_component: ChaseComponent
+
+func _physics_process(delta: float) -> void:
+  chase_component.chase_behaviour(delta)
